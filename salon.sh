65ae76797858cd762049e9be2acb47c41307c5c0
toroienoo <91039987+vedasishere@users.noreply.github.com>
2023-04-08 13:50:33 +0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -11,21 +11,19 @@ MAIN_MENU() {
   SERVICES=$($PSQL "select * from services")
   echo "$SERVICES" | while IFS=" | " read SERVICE_ID SERVICE_NAME
   do
-    # if [[ $SERVICE_ID =~ ^[0-9]+$ ]]
-    # then
-      echo "$SERVICE_ID) $SERVICE_NAME"
-    # fi
+    echo "$SERVICE_ID) $SERVICE_NAME"
   done
   SELECT_SERVICE
 }
 SELECT_SERVICE() {
   read SERVICE_ID_SELECTED
   FIND_SERVICE_ID=$($PSQL "select service_id from services where service_id=$SERVICE_ID_SELECTED")
-  echo $FIND_SERVICE_ID
   if [[ -z $FIND_SERVICE_ID ]]
   then
-    MAIN_MENU "I could not find that service. What would you like today?"
+    MAIN_MENU "\nI could not find that service. What would you like today?"
   else
+    QUERY_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED")
+    SERVICE_NAME=$(echo $QUERY_NAME | sed -r "s/^ *| *$//g")
     echo -e "\nWhat's your phone number?"
     read CUSTOMER_PHONE
     CUSTOMER_NAME=$($PSQL "select name from customers where phone='$CUSTOMER_PHONE'")
@@ -33,10 +31,16 @@ SELECT_SERVICE() {
     then
       echo -e "\nI don't have a record for that phone number, what's your name?"
       read CUSTOMER_NAME
+      NAME=$(echo "$CUSTOMER_NAME" | sed -r "s/^ *| *$//g")
+      INSERT_CUSTOMER=$($PSQL "insert into customers(name, phone) values('$NAME', '$CUSTOMER_PHONE')")
+    else
+      NAME=$(echo "$CUSTOMER_NAME" | sed -r "s/^ *| *$//g")
     fi
-    echo -e "\nWhat time would you like your cut, $CUSTOMER_NAME?"
+    CUSTOMER_ID=$($PSQL "select customer_id from customers where name='$NAME'")
+    echo -e "\nWhat time would you like your $SERVICE_NAME, $NAME?"
     read SERVICE_TIME
-    echo -e "\nI have put you down for a cut at $SERVICE_TIME, $CUSTOMER_NAME."
+    INSERT_APPOINT=$($PSQL "insert into appointments(customer_id, service_id, time) values($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $NAME.\n"
   fi
 }
 MAIN_MENU

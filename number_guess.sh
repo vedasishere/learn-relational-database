39cab03a90a6f194f6baca2ab6daa94c4ab0b1fb
toroienoo <91039987+vedasishere@users.noreply.github.com>
2023-04-11 21:40:53 +0700

    Create number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 RANDOM_NUMBER=$(($RANDOM % 1000 + 1))
-echo "random: $RANDOM_NUMBER"
 echo "Enter your username:"
 read USERNAME
 CHECK_USER=$($PSQL "select * from users where name='$USERNAME'")
